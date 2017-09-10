731f90b1ef6abd79975715b046c281c2013de97c
Marianna Foos <marianna.foos@gmail.com>
2017-09-09 21:10:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### It's decorative emoji season, mofos!
+
+Source it in your .bashrc and get a new emoji-prompt at the turn of the (meteorological) seasons, plus a handful of odd holidays with static dates.
---
+++ festive_prompt.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+os_stuff=':\W \u '
+
+case `date +%m` in
+    03|04|05)
+        emoji=🌷  ;;
+    06|07|08)
+        emoji=☀️  ;;
+    09|10|11)
+        emoji=🍂  ;;
+    12|01|01)
+        emoji=❄️  ;;
+esac
+
+case `date +%m-%d` in
+    06-01)
+        emoji=🎂  ;;
+    01-01)
+        emoji=🎉  ;;
+    02-14)
+        emoji=💝  ;;
+    03-17)
+        emoji=☘️  ;;
+    04-01)
+        emoji=😆  ;;
+    04-15)
+        emoji=🐱  ;;
+    07-01)
+        emoji=🍁  ;;
+    07-04)
+        emoji=🇺🇸  ;;
+    10-31)
+        emoji=🎃  ;;
+    12-31)
+        emoji=🍾  ;;
+    
+esac
+        
+if [ -n "$emoji" ]; then
+    PS1="$emoji $os_stuff"    
+fi
